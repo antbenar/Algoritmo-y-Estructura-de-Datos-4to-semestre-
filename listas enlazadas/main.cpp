eb915b5c986d6e465ef2962ea468ca4e7c3be4f9
antbenar <antbenar1999@gmail.com>
2017-09-16 15:18:23 -0500

    Update main.cpp

--- listas enlazadas/main.cpp
+++ listas enlazadas/main.cpp
@@ -6,7 +6,7 @@ class nodo{
 public:
 	T dato;
 	nodo* next;
-
+	
 	nodo(T dato_){
 		dato=dato_;
 		next=NULL;
@@ -18,61 +18,65 @@ class Lista{
 public:
 	nodo<T> *head;
 	int num_nodos;
-
+	
 	Lista(T dato_)
-    {
-        head = new nodo<T> (dato_);
-        num_nodos++;
-    }
-
+	{
+		head = new nodo<T> (dato_);
+		num_nodos++;
+	}
+	
 	bool buscar(T dato_, nodo<T>**& nodo_devuelto){
-        nodo<T> **temp=&head;                         cout<<(*temp)->dato<<"  dato inicial"<<endl;
-        while(*temp!=NULL && (*temp)->dato<=dato_){
-            *temp=(*temp)->next;                        cout<<(*temp)->dato<<  "  whileeeeee"<<endl;
-        }
-        nodo_devuelto=temp;             cout<<(*temp)->dato<<  "  (*temp)->dato"<<endl;
-        if((*temp)==NULL){
-            return 0;
-        }
-        return 1;
+		nodo<T> **temp=&head;                      
+		
+		while(*(temp)!=NULL && (*temp)->dato<dato_){
+			temp=&((*temp)->next);                      
+		}
+		nodo_devuelto=temp;       
+		if((*temp)==NULL || (*temp)->dato!=dato_){
+			return false;
+		}
+		return true;
 	}
-
-	void insertar(T dato_, nodo<T>**& next_insert){
-	    nodo<T>* aux=new nodo<T>(dato_);
-		aux->next=*next_insert;
-		*next_insert=aux;
+	
+	void insertar(T dato_){
+		nodo<T>** next_insert;
+		if (!buscar(dato_,next_insert)){
+			nodo<T>* aux=new nodo<T>(dato_);
+			aux->next=*next_insert;
+			*next_insert=aux;
+		}
 	}
-
-    void borrar(nodo<T>**& nodo_borrar){
-	    nodo<T>* aux= (*nodo_borrar)->next;
-        delete (*nodo_borrar);
-        *nodo_borrar=aux;
+	
+	void borrar(T dato_){
+		nodo<T>** nodo_borrar;
+		if (buscar(dato_,nodo_borrar)){
+			nodo<T>* aux= (*nodo_borrar)->next;
+			delete (*nodo_borrar);
+			*nodo_borrar=aux;
+		}
 	}
 };
 
 int main(int argc, char *argv[]) {
 	Lista<int> lista(6);
-	nodo<int> **next_= &(lista.head);
-	lista.insertar(5,next_);
-	lista.insertar(4,next_);
-    //lista.borrar(next_);
-
-    /*int dato=5;
-    cout<<lista.buscar(dato,next_)<<endl;
-    lista.insertar(dato,next_);*/
-
+	lista.insertar(5);
+	lista.insertar(3);
+	lista.insertar(7);
+	lista.borrar(9);
+	lista.borrar(5);
+	
 	nodo<int> *temp=lista.head;
 	while(temp!=NULL){
-        cout<<temp->dato<<", ";
-        temp=temp->next;
+		cout<<temp->dato<<", ";
+		temp=temp->next;
 	}cout<<endl;
 
-    while(temp!=NULL){
-        temp=lista.head;
-        lista.head=lista.head->next;
-        delete temp;
+	while(temp!=NULL){
+		temp=lista.head;
+		lista.head=lista.head->next;
+		delete temp;
 	}cout<<endl;
-
-
+	
+	
 	return 0;
 }
